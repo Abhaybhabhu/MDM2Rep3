e161812ce215b5a921759cf64c4a9d027289bc48
lusCombeNCut <124700526+lusCombeNCut@users.noreply.github.com>
2024-04-18 15:35:38 +0100

    Add files via upload

--- prophetModelCrossValidation.py
+++ prophetModelCrossValidation.py
@@ -1,9 +1,19 @@
 import pandas as pd
-from prophet import Prophet
+import itertools
+import numpy as np
+import prophet
+from prophet.diagnostics import cross_validation
+import prophet.diagnostics
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
+from prophet.plot import plot_plotly, plot_components_plotly
 
-data_path = 'august.csv'
+data_path = 'october.csv'
+
+param_grid = {  
+    'changepoint_prior_scale': [0.02, 0.01, 0.03, 0.04],
+    'seasonality_prior_scale': [0.08, 0.1, 0.12, 0.14],
+}
 
 def load_and_preprocess_data(data_path):
     """Loads data from CSV, aggregates counts, and extracts features."""
@@ -13,17 +23,38 @@ def load_and_preprocess_data(data_path):
     df_aggregated['hour'] = df_aggregated['ds'].dt.hour
     df_aggregated['day_of_week'] = df_aggregated['ds'].dt.dayofweek
     df_aggregated['is_weekend'] = (df_aggregated['ds'].dt.dayofweek >= 5).astype(int)
+    df_aggregated['reading_week'] = ([0] *96*29 + [1, 1] *96)
+    print(df_aggregated.info())
     return df_aggregated
 
 def create_and_fit_model(df_train, daily_fourier_order=15):
     """Initializes, configures, and fits the Prophet model."""
-    m = Prophet(changepoint_prior_scale=0.02, seasonality_prior_scale=0.08)
+
+        # Generate all combinations of parameters
+    all_params = [dict(zip(param_grid.keys(), v)) for v in itertools.product(*param_grid.values())]
+    rmses = []  # Store the RMSEs for each params here
+
+    # Use cross validation to evaluate all parameters
+    for params in all_params:
+        m = prophet.Prophet(**params).fit(df_train)  # Fit model with given params
+        df_cv = prophet.diagnostics.cross_validation(m, horizon='5 days',initial="10 days", parallel="processes")
+        df_p = prophet.diagnostics.performance_metrics(df_cv, rolling_window=1)
+
+        rmses.append(df_p['rmse'].values[0])
+
+    # Find the best parameters
+    tuning_results = pd.DataFrame(all_params)
+    tuning_results['rmse'] = rmses
+    print(tuning_results)
+
+    m = prophet.Prophet(changepoint_prior_scale=5, seasonality_prior_scale=5)
     m.add_seasonality(name='daily', period=1, fourier_order=daily_fourier_order)
     m.add_seasonality(name='hourly', period=1/24, fourier_order=20)
     m.add_seasonality(name='weekly', period=7, fourier_order=10)
     m.add_regressor('hour')
     m.add_regressor('day_of_week')
     m.add_regressor('is_weekend')
+    #m.add_regressor('reading_week')
     m.fit(df_train)
     return m
 
@@ -33,6 +64,7 @@ def make_and_plot_forecast(model, df_test):
     future['hour'] = future['ds'].dt.hour
     future['day_of_week'] = future['ds'].dt.dayofweek
     future['is_weekend'] = (future['ds'].dt.dayofweek >= 5).astype(int)
+    #future['reading_week'] = ([0] *96*29 + [1, 1] *96)
     forecast = model.predict(future)
     
     # Set negative predicted values to zero
@@ -45,10 +77,7 @@ def make_and_plot_forecast(model, df_test):
     plt.xlabel('Date')
     plt.ylabel('Total Pedestrian Count')
     plt.show()
-    fig = model.plot_components(forecast)
-    plt.show()
-
-    
+    plot_plotly(model, forecast)
     return forecast
 
 
